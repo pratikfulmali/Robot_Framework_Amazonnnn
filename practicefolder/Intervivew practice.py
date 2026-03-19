e6802628839b7f7f852c6332c284fb37860c513b
PRATIK FULMALI <fulmalipratik79@gmail.com>
2026-03-19 21:25:58 +0530

    Dictonary and list highest no Prog

---
+++ practicefolder/Intervivew practice.py
@@ -0,0 +1,22 @@
+#Find the most expensive phone from the given list of phones and their prices.
+
+low=0
+Device={
+    "samsung":550000,
+    "MI":30000,
+    "Xiomi":90000,
+    "I-Phone":180000
+    }
+for phone,price in Device.items():
+    if price>low:
+        low=price
+        expDevice=phone
+print(expDevice)
+
+
+ #Find the second highest from below list
+
+l2=[3,4,6,7,8,9,33,23,45,66]
+l2==l2.sort()
+print(l2)
+print(l2[-2])
